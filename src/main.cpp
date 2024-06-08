9b3a1d7137ad5c2fc7b22c4156141acf4dcaa996
Ban1p7 <112499126+Ban1p7@users.noreply.github.com>
2024-06-08 18:43:04 +0100

    Switched to Raylib & added texture

--- src/main.cpp
+++ src/main.cpp
@@ -1,60 +1,45 @@
-#include <SDL.h>
-#include <iostream>
-
-// Window Class
-#include "window.hpp"
-
-// Player Class
+#include "raylib.h"
 #include "player.cpp"
 
-// Run this to start the game
-int main(){
-  // Define the window
-  Window GameWindow;
-  init_window(&GameWindow);
-  int screenWidth;
-  int screenHeight;
-  int gravity = 1;
-  bool flip = false;
-  bool pressed = false;
-  SDL_GetWindowSize(GameWindow.SDLWindow, &screenWidth, &screenHeight);
-  SDL_Event event;
-  SDL_Color yellow = {255, 204, 0, 255};
-  SDL_Rect nullRect = {0, 0, 0, 0};
-  Player player = {0, 0, 32, 32, nullRect, yellow};
-  player.posRect.w = player.posRect.h = 32;
-  bool running = true;
-  int start = event.quit.timestamp;
-  while (running) {
-    SDL_PollEvent(&event);
-    if (event.type == SDL_QUIT){
-      running = false;
-    }
-    if (event.type == SDL_KEYUP){
-//       Debug
-//       std::cout << "Key up!" << std::endl;
-      pressed = false;
-    }
-    if (event.type == SDL_KEYDOWN && pressed == false){
-//       Debug
-//       std::cout << "Key down!" << std::endl;
-      flip = true;
-      pressed = true;
-    }
-    if (flip == true){
+
+int main(void){
+    const int screenWidth = 800;
+    const int screenHeight = 600;
+
+    InitWindow(screenWidth, screenHeight, "Didactic Chainsaw");
+
+    int gravity = 5;
+
+    Player player = {0, 0, 64, 64};
+    player.sprite = LoadTexture(ASSETS_PATH"klerb.png");
+
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()){
+    // Update stuff
+
+    if (IsMouseButtonPressed(0)){
       gravity *= -1;
-      flip = false;
     }
+
     player.y += gravity;
+
     if (player.y > screenHeight - player.height){
       player.y = screenHeight - player.height;
     }
     if (player.y < 0){
       player.y = 0;
     }
-    updatePlayerRect(&player);
-    RenderFrame(&GameWindow, &player);
-  }
-  SDL_Quit();
-  return 0;
+
+    // Draw everything
+        BeginDrawing();
+
+            ClearBackground(RAYWHITE);
+            DrawTexture(player.sprite, player.x, player.y, WHITE);
+
+        EndDrawing();
+    }
+    CloseWindow();
+
+    return 0;
 }
--- src/player.cpp
+++ src/player.cpp
@@ -1,18 +1,12 @@
 #ifndef playerdotcpp
 #define playerdotcpp
 
+#include "raylib.h"
+
 struct Player {
   int x, y, width, height;
-  SDL_Rect posRect;
-  SDL_Color color;
+//   SDL_Rect posRect;
+  Texture2D sprite;
 };
-
-void updatePlayerRect(Player* player){
-  player->posRect.x = player->x;
-  player->posRect.y = player->y;
-  player->posRect.w = player->width;
-  player->posRect.h = player->height;
-  return;
-}
 
 #endif
